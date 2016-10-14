7b3a1ee1600c9ebe0d67a5351cdada5807eddaf2
laserman <mherweg@das-labor.org>
2016-10-14 19:01:01 +0200

    Y12B.ac: moved the door from right to left side of the aircraft. removed unused UV coordinates. 3.5MB -> 2.7MB

--- start5.sh
+++ start5.sh
@@ -3,9 +3,7 @@ SIM_HZ=20
 MP_HZ=20
 MP_SERVER=i3
 
-
-
-FG_HOME=/SDB2/dot-fgfs fgfs --prop:/sim/frame-rate-throttle-hz=$SIM_HZ  --fg-aircraft=/mh/submodules/Aircraft --aircraft=Walker-mp --airport=ZHCC --timeofday=noon --fg-scenery=/mh/scenery/2.0 --multiplay=in,$MP_HZ,,5001 --multiplay=out,$MP_HZ,$MP_SERVER,5000 --callsign=1 --altitude=5000 &
+#FG_HOME=/SDB2/dot-fgfs fgfs --prop:/sim/frame-rate-throttle-hz=$SIM_HZ  --fg-aircraft=/mh/submodules/Aircraft --aircraft=Walker-mp --airport=ZHCC --timeofday=noon --fg-scenery=/mh/scenery/2.0 --multiplay=in,$MP_HZ,,5001 --multiplay=out,$MP_HZ,$MP_SERVER,5000 --callsign=1 --altitude=5000 &
 
 FG_HOME=/mh/dot-fgfs2  fgfs --prop:/sim/frame-rate-throttle-hz=$SIM_HZ  --fg-aircraft=/mh/submodules/Aircraft --aircraft=Walker-mp --airport=ZHCC --timeofday=noon --fg-scenery=/mh/scenery/2.0 --multiplay=in,$MP_HZ,,5002 --multiplay=out,$MP_HZ,$MP_SERVER,5000 --callsign=2 --altitude=5003 &
 
